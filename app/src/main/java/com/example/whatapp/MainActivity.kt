05fb729430f3e845e73a24a74bc2530adaa645e1
CYRIL ANGELO FILOMENO <cyrilangelo.filomeno.cics@ust.edu.ph>
2023-11-23 01:21:09 +0800

    Initial commit

---
+++ app/src/main/java/com/example/whatapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.whatapp
+
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val redirectButton: Button = findViewById(R.id.Start)
+
+        redirectButton.setOnClickListener {
+            // Start the SecondActivity
+            val intent = Intent(this@MainActivity, crudtab::class.java)
+            startActivity(intent)
+        }
+
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+    }
+}
---
+++ app/src/main/java/com/example/whatapp/crudtab.kt
@@ -0,0 +1,36 @@
+package com.example.whatapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class crudtab : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.crudtab)
+
+        val customButton1 : Button = findViewById(R.id.AddButton)
+        val customButton2 : Button = findViewById(R.id.DeleteButton)
+        val customButton3 : Button = findViewById(R.id.RandomizeButton)
+
+
+        customButton1.setBackgroundResource(R.drawable.rounded_button_background)
+        customButton1.setTextColor(resources.getColor(R.color.greenfont))
+        customButton2.setBackgroundResource(R.drawable.rounded_button_background)
+        customButton2.setTextColor(resources.getColor(R.color.greenfont))
+        customButton3.setBackgroundResource(R.drawable.rounded_button_background)
+        customButton3.setTextColor(resources.getColor(R.color.greenfont))
+
+        customButton3.setOnClickListener {
+            // Start the SecondActivity
+            val intent = Intent(this@crudtab, resulttab::class.java)
+            startActivity(intent)
+        }
+
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+    }
+}
